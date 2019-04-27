3695191e8972be0f394c1dc5f216ac8378088031
Diederik de Groot <dkgroot@talon.nl>
2019-04-27 17:43:37 +0200

    Extend example a little

--- example.php
+++ example.php
@@ -8,11 +8,22 @@ require(implode(DIRECTORY_SEPARATOR, array(
 
 use CiscoXML\CiscoXMLService;
 use CiscoXML\Forms\CiscoIPPhoneText;
+use CiscoXML\Forms\CiscoIPPhoneMenu;
 
 // implementation
-$obj = new CiscoIPPhoneText();
-$obj->setTitle("Text Title");
-$obj->setPrompt("Prompt Message");
-$obj->setText("This is where body text can be inserted for displaying custom data");
-echo $obj->toXML();
+echo "\n=text=====\n";
+$textobj = new CiscoIPPhoneText();
+$textobj->setTitle("Text Title");
+$textobj->setPrompt("Prompt Message");
+$textobj->setText("This is where body text can be inserted for displaying custom data");
+echo $textobj->toXML();
+echo "============\n";
+echo "\n=menu item=====\n";
+$menuobj = new CiscoIPPhoneMenu();
+$menuobj->setTitle("Menu Title");
+$menuobj->setPrompt("Prompt Message");
+$menuobj->addItem("Item1", "http://example.com/URL1.php");
+$menuobj->addItem("Item2", "http://example.com/URL2.php");
+echo $menuobj->toXML();
+echo "============\n";
 ?>
